689c91a54c7dedffeaf9b045f6323c274f30c5a3
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-07-04 16:18:07 +0200

    Done html layout, working algorithm implementation

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,23 @@
+const fizzBuzzContainer = document.getElementById("fizz_buzz_container");
+
+
+
+
+
+
 // Main algorithm
 
 for (i = 1; i <= 100; i++) {
 
+    const div = `<div>${i}</div>`;
+    const divFizz = `<div>Fizz</div>`;
+    const divBuzz = `<div>Buzz</div>`;
+    const divFizzBuzz = `<div>FizzBuzz</div>`;
+
     if (i % 3 === 0 && i % 5 === 0) {
 
         console.log("FizzBuzz");
+        fizzBuzzContainer.innerHTML += 
 
     } else if (i % 3 === 0) {
 
@@ -16,7 +29,7 @@ for (i = 1; i <= 100; i++) {
 
     } else {
 
-        console.log(i)
+        console.log(i);
 
     }
     
